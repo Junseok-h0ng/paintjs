a1681a8af7747e184885055de81a6cac166b04e8
Junseok Hong <wnstjr6467@gmail.com>
2020-02-20 19:26:45 +0900

    Drawing Canvas

---
+++ app.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("jsCanvas")
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function handleMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleMouseDown() {
+    painting = true;
+}
+
+function stopEvent() {
+    painting = false;
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", stopEvent);
+    canvas.addEventListener("mouseleave", stopEvent);
+}
